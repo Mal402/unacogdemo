9e26346fe9f33bbc84f493d789b4e9855a6333a7
HuskerSam <sam.huelsdonk@gmail.com>
2024-03-31 18:28:43 -0500

    row table options

--- public/metricext/metricext.js
+++ public/metricext/metricext.js
@@ -34,10 +34,49 @@ async function main() {
         let promptTemplateList = await promptsTable.getData();
         chrome.storage.local.set({promptTemplateList});
     });
-    promptsTable.on("cellClick", (e, cell) => {
-        if (column.getField() === "delete") {
+    promptsTable.on("rowMoved", async (cell) => {
+        let promptTemplateList = await promptsTable.getData();
+        chrome.storage.local.set({promptTemplateList});
+    });
+    promptsTable.on("cellClick", async (e, cell) => {
+        if (cell.getColumn().getField() === "delete") {
             promptsTable.deleteRow(cell.getRow());
+            let promptTemplateList = await promptsTable.getData();
+        chrome.storage.local.set({promptTemplateList});
         }
+    });
+    let addButton = document.querySelector('.add_row');
+    addButton.addEventListener('click', async () => {
+        promptsTable.addRow({id: '', description: '', prompt: ''});
+    });
+    let exportButton = document.querySelector('.export_rows');
+    exportButton.addEventListener('click', async () => {
+        let promptTemplateList = await promptsTable.getData();
+        let blob = new Blob([JSON.stringify(promptTemplateList)], {type: "application/json"});
+        let url = URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = 'promptTemplateList.json';
+        a.click();
+        URL.revokeObjectURL(url);
+    });
+    let importButton = document.querySelector('.import_rows');
+    importButton.addEventListener('change', async (e) => {
+        let file = e.target.files[0];
+        let reader = new FileReader();
+        reader.onload = async (e) => {
+            let promptTemplateList = JSON.parse(e.target.result);
+            promptsTable.setData(promptTemplateList);
+            chrome.storage.local.set({promptTemplateList});
+            importButton.value = '';
+        };
+        reader.readAsText(file);
+    });
+
+    let resetButton = document.querySelector('.reset_default');
+    resetButton.addEventListener('click', async () => {
+        promptsTable.setData(defaultPrompts);
+        chrome.storage.local.set({promptTemplateList: defaultPrompts});
     });
 }
 main();
